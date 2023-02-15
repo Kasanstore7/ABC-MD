fb29f91fc14c828bad47bbb7b33b48e8d0ceb4b2
Kasanstore7 <114809701+Kasanstore7@users.noreply.github.com>
2023-02-15 16:21:22 +0700

    Apacoba

--- main.js
+++ main.js
@@ -86,7 +86,7 @@ global.loadDatabase = async function loadDatabase() {
 }
 loadDatabase()
 
-global.authFile = `${opts._[0] || 'session'}.data.json`
+global.authFile = `${opts._[0] || 'ABC'}.data.json`
 const { state, saveState } = store.useSingleFileAuthState(global.authFile)
 
 const connectionOptions = {
